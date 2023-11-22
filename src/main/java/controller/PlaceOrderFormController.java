05cf461c891e0f8a42c12b45a53611c34daa1f84
shenmadu <tashen.madu@gmail.com>
2023-11-22 16:44:46 +0530

    Uploard project

---
+++ src/main/java/controller/PlaceOrderFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class PlaceOrderFormController {
+
+    public JFXButton goBackButton;
+
+    public void goBackButtonOnAction(ActionEvent actionEvent) {
+        Stage stage=(Stage) goBackButton.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/DashboardForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
